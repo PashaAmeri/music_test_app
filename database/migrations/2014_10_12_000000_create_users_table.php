2967ad41e8edc4449005c1a7e8a88da229fe7482
PashaAmeri <a.pasha.ameri081@gmai.com>
2022-11-30 00:47:14 +0330

    updated user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', [ROLE_USER, ROLE_ARTIST, ROLE_ADMIN]);
+            $table->string('name')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('password')->nullable();
+            $table->
             $table->timestamps();
         });
     }

73c12a317f92e660bc01e3f8d4be1781bc2c846e
PashaAmeri <a.pasha.ameri081@gmai.com>
2022-11-30 01:51:48 +0330

    comment

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+
+// user roles constants
 const ROLE_USER = '3';
 const ROLE_ARTIST = '2';
 const ROLE_ADMIN = '1';
@@ -26,7 +28,7 @@ function randomString(int $lenth = 6, string $type = NULL)
 
     for ($i = 0; $i < $lenth; $i++) {
         
-        $randstring = $characters[rand(0, strlen($characters))];
+        $randstring = $characters[rand(0, strlen($characters) - 1)];
     }
 
     return $randstring;

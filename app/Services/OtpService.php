0d74bbba6a955f53bd22c806fb809c0d13ef958a
PashaAmeri <a.pasha.ameri081@gmai.com>
2022-11-30 01:56:07 +0330

    fixed interface issue & error handling to gen code

--- app/Services/OtpService.php
+++ app/Services/OtpService.php
@@ -3,8 +3,9 @@
 namespace App\Services;
 
 use App\Models\User;
-use App\Interfaces\OtpCodeRepositoryInterface;
 use Illuminate\Http\Request;
+use App\Repositories\OtpCodeRepository;
+use App\Interfaces\OtpCodeRepositoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class OtpService
@@ -12,7 +13,7 @@ class OtpService
 
     private OtpCodeRepositoryInterface $otpCode;
 
-    public function __construct(OtpCodeRepositoryInterface $otpCode)
+    public function __construct(OtpCodeRepository $otpCode)
     {
         
         $this->otpCode = $otpCode;
@@ -21,8 +22,16 @@ class OtpService
     public function generateCode(Request $request)
     {
 
-        $this->otpCode->create(randomString(6, 'numeric'), User::where('phone', $request->only('phone')));
-        
+        try {
+            
+            $this->otpCode->create(randomString(6, 'numeric'), User::where('phone', $request->phone)->first());
+        } catch (\Throwable $th) {
+            
+            return response([
+                'message' => 'oops! Something went wrong.'
+            ], Response::HTTP_CONFLICT);
+        }
+
         return response([
             'message' => 'Code Generated.'
         ], Response::HTTP_CREATED);

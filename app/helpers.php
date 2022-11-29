069b489f5acf10ab9c152e554daa39b3290c338f
PashaAmeri <a.pasha.ameri081@gmai.com>
2022-11-29 20:46:50 +0330

    added helpers.php file to app

---
+++ app/helpers.php
@@ -0,0 +1,33 @@
+<?php
+
+const ROLE_USER = '3';
+const ROLE_ARTIST = '2';
+const ROLE_ADMIN = '1';
+
+function RandomString(int $lenth = 6, string $type = NULL)
+{
+
+    switch ($type) {
+        case 'numeric':
+            
+            $characters = '0123456789';
+            break;
+        case 'alpha':
+            
+            $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            break;
+        default:
+            
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            break;
+    }
+
+    $randstring = '';
+
+    for ($i = 0; $i < $lenth; $i++) {
+        
+        $randstring = $characters[rand(0, strlen($characters))];
+    }
+
+    return $randstring;
+}
